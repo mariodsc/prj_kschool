e54dcda021b4d99c9a55a9fcf1bfdc8040223ad8
mariodsc <mdesousac@gmail.com>
2021-01-22 19:24:55 +0000

    Build model

--- trainer/task.py
+++ trainer/task.py
@@ -19,6 +19,15 @@ def _preprocess_data(x, y):
     y = to_categorical(y)
     return x,y
 
+def _build_model():
+    m = models.Sequential()
+    m.add(layers.Input((28,28), name='my_input_layer'))
+    m.add(layers.Flatten())
+    m.add(layers.Dense(128, activation=activations.relu))
+    m.add(layers.Dense(64, activation=activations.relu))
+    m.add(layers.Dense(32, activation=activations.relu))
+    m.add(layers.Dense(10, activation=activations.softmax))
+    return m
 
 def train_and_evaluate(batch_size, epochs, job_dir, output_path):
     # Donwload the data
@@ -29,7 +38,8 @@ def train_and_evaluate(batch_size, epochs, job_dir, output_path):
     x_test, y_test = _preprocess_data(x_test, y_test)
 
     # Build the model
-
+    m = _build_model()
+    
     # Train the model
 
     # Evaluate the model
